6c9f80462e4460efe64fb1e8500048c51f394b53
sempreverde1919 <110019528+sempreverde1919@users.noreply.github.com>
2023-04-12 11:08:29 +0000

    Task 6 - test3

--- project-calculator/script.js
+++ project-calculator/script.js
@@ -24,16 +24,16 @@ function divide(num1, num2) {
 function operate(num1, operator, num2){
     switch(operator){
         case "+":
-            add(num1, num2);
+            return add(num1, num2);
             break;
         case "-":
-            subtract(num1, num2);
+            return subtract(num1, num2);
             break;
         case "*":
-            multiply(num1, num2);
+            return multiply(num1, num2);
             break;
         case "/":
-            divide(num1, num2);
+            return divide(num1, num2);
             break;
         default:
             console.log("No such oparation")
