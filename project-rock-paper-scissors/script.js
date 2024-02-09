2e0528c5845aa94a84cb3e421aed910702c152f7
sempreverde1919 <110019528+sempreverde1919@users.noreply.github.com>
2024-02-09 10:36:40 +0000

    Final version

--- project-rock-paper-scissors/script.js
+++ project-rock-paper-scissors/script.js
@@ -1,26 +1,53 @@
+let playerPoints = 0;
+let computerPoints = 0;
+playGame();
+
+function playGame(){
+  for(let i=0; i<5; i++){
+    let playerSelection = prompt("Rock, paper or scissors?");
+    let computerSelection = getComputerChoice();
+    console.log(playRound(playerSelection, computerSelection));
+  }
+  console.log(getWinner());
+}
+
 function getComputerChoice(){
-    let choice = Math.floor(Math.random()*3);
-    switch(choice){
-      case 0:
-        return "Rock";
-      case 1:
-        return "Paper";
-      case 2:
-        return "Scissors";
-    }
+  let choice = Math.floor(Math.random()*3);
+  switch(choice){
+    case 0:
+      return "rock";
+    case 1:
+      return "paper";
+    case 2:
+      return "scissors";
   }
-  
+}
+
 function playRound(playerSelection, computerSelection){
-    playerSelection = playerSelection.toLowerCase();
-    if(playerSelection === "rock" && computerSelection === "scissors"
-      || playerSelection === "paper" && computerSelection === "rock"
-      || playerSelection === "scissors" && computerSelection === "paper"){
-      return `You win! ${playerSelection} beats ${computerSelection}.`
-    } else if (computerSelection === "rock" && playerSelection === "scissors"
-      || computerSelection === "paper" && playerSelection === "rock"
-      || computerSelection === "scissors" && playerSelection === "paper"){
-          return `You lose! ${computerSelection} beats ${playerSelection}.`
-    } else{
-      return "Tie!"
-    }
+  playerSelection = playerSelection.toLowerCase();
+  if(playerSelection === "rock" && computerSelection === "scissors"
+    || playerSelection === "paper" && computerSelection === "rock"
+    || playerSelection === "scissors" && computerSelection === "paper"){
+    playerPoints++;
+    return `You win! ${playerSelection} beats ${computerSelection}.
+    Player: ${playerPoints} - Computer: ${computerPoints}`
+  } else if (computerSelection === "rock" && playerSelection === "scissors"
+    || computerSelection === "paper" && playerSelection === "rock"
+    || computerSelection === "scissors" && playerSelection === "paper"){
+    computerPoints++;    
+    return `You lose! ${computerSelection} beats ${playerSelection}.
+    Player: ${playerPoints} - Computer: ${computerPoints}`
+  } else{
+    return "Tie!"
   }
+}
+
+function getWinner(){
+  if(playerPoints>computerPoints){
+    return "The final winner is the player!"
+  } else if (computerPoints>playerPoints){
+    return "The final winner is the computer!"
+  } else{
+    return "The final result is tie!"
+  }
+}
