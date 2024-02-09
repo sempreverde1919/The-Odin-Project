a1018ef1003e16a2dc1479b85794c4199139fa3d
sempreverde1919 <110019528+sempreverde1919@users.noreply.github.com>
2024-02-09 10:10:24 +0000

    Function of a single round is complete

--- project-rock-paper-scissors/script.js
+++ project-rock-paper-scissors/script.js
@@ -9,3 +9,18 @@ function getComputerChoice(){
         return "Scissors";
     }
   }
+  
+function playRound(playerSelection, computerSelection){
+    playerSelection = playerSelection.toLowerCase();
+    if(playerSelection === "rock" && computerSelection === "scissors"
+      || playerSelection === "paper" && computerSelection === "rock"
+      || playerSelection === "scissors" && computerSelection === "paper"){
+      return `You win! ${playerSelection} beats ${computerSelection}.`
+    } else if (computerSelection === "rock" && playerSelection === "scissors"
+      || computerSelection === "paper" && playerSelection === "rock"
+      || computerSelection === "scissors" && playerSelection === "paper"){
+          return `You lose! ${computerSelection} beats ${playerSelection}.`
+    } else{
+      return "Tie!"
+    }
+  }
