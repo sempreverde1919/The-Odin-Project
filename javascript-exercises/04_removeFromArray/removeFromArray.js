dd30ba5c0a1075d2befc78541979787aca9af435
sempreverde1919 <110019528+sempreverde1919@users.noreply.github.com>
2024-02-12 11:43:47 +0000

    Solution of removeFromArray.js

--- javascript-exercises/04_removeFromArray/removeFromArray.js
+++ javascript-exercises/04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+    let res = [];
+    arr.forEach((item) => {
+    if (!args.includes(item)) {
+      res.push(item);
+    }});
+    return res;
 };
 
 // Do not edit below this line
