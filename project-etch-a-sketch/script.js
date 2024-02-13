8c05e7c311441a3175042751f13a9c5b21238890
sempreverde1919 <110019528+sempreverde1919@users.noreply.github.com>
2024-02-13 14:55:08 +0000

    Final version

--- project-etch-a-sketch/script.js
+++ project-etch-a-sketch/script.js
@@ -1,10 +1,17 @@
 const container = document.querySelector('.container');
+const button = document.querySelector('button');
+let size = 16;
+button.addEventListener('click', changeGrid);
 
-for(let i=1; i<=16*16; i++){
-  const square = document.createElement('div');
-  square.classList.add('square');
-  container.appendChild(square);
-  square.addEventListener('mouseover', e=>hoverChange(e))
+function initialize(){
+  container.textContent="";
+  for(let i=1; i<=size*size; i++){
+    const square = document.createElement('div');
+    square.classList.add('square');
+    square.style.flexBasis = `${100/size}%`;
+    container.appendChild(square);
+    square.addEventListener('mouseover', e=>hoverChange(e))
+  }
 }
 
 function hoverChange(square){
@@ -12,3 +19,11 @@ function hoverChange(square){
   square.target.style.backgroundColor = random;
 }
 
+function changeGrid(){
+  do{
+    size = prompt("Add the size of the grid:");
+  } while(isNaN(+size) || +size>100);
+  initialize();
+}
+
+initialize();
