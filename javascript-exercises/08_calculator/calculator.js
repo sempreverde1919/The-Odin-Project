8fe2e4426db5151761f5440d923077119c64fecd
sempreverde1919 <110019528+sempreverde1919@users.noreply.github.com>
2024-02-14 13:44:33 +0100

    Update calculator.js

--- javascript-exercises/08_calculator/calculator.js
+++ javascript-exercises/08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((x, y) => x+y, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+ return arr.reduce((x, y) => x*y);
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	return Math.pow(x, y);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num < 0) return -1;
+  else if (num == 0) return 1;
+  else {
+    return (num * factorial(num - 1));
+  }
 };
 
 // Do not edit below this line
