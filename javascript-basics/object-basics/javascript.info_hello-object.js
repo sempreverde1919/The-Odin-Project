a9edd6d6b07645d27370fe8b1931bd1305714ffa
sempreverde1919 <110019528+sempreverde1919@users.noreply.github.com>
2024-02-14 08:40:41 +0100

    Update javascript.info_hello-object.js

--- javascript-basics/object-basics/javascript.info_hello-object.js
+++ javascript-basics/object-basics/javascript.info_hello-object.js
@@ -1,1 +1,10 @@
-
+// 1. Create an empty object user.
+let user = {};
+// 2. Add the property name with the value John.
+user.name = "John";
+// 3. Add the property surname with the value Smith.
+user.surname = "Smith";
+// 4. Change the value of the name to Pete.
+user.name = "Pete";
+// 5. Remove the property name from the object.
+delete user.name;
