450fd479edd17cae4dc5cec16929f790fa2f1adb
sempreverde1919 <110019528+sempreverde1919@users.noreply.github.com>
2024-02-14 15:40:06 +0100

    Create javascript.info_shuffle-an-array.js

---
+++ javascript-basics/object-basics/javascript.info_shuffle-an-array.js
@@ -0,0 +1,23 @@
+// https://javascript.info/array-methods#shuffle-an-array
+// Shuffle an array
+
+/*
+Write the function shuffle(array) that shuffles (randomly reorders) elements of the array.
+
+Multiple runs of shuffle may lead to different orders of elements. For instance:
+
+let arr = [1, 2, 3];
+shuffle(arr);
+// arr = [3, 2, 1]
+shuffle(arr);
+// arr = [2, 1, 3]
+shuffle(arr);
+// arr = [3, 1, 2]
+// ...
+
+All element orders should have an equal probability. For instance, [1,2,3] can be reordered as [1,2,3] or [1,3,2] or [3,1,2] etc, with equal probability of each case.
+*/
+
+function shuffle(arr){
+  arr.sort(() => Math.random() - 0.5);
+}
