de1b1ebd6844407773039f4e1c4f9e576b603f4f
sempreverde1919 <110019528+sempreverde1919@users.noreply.github.com>
2024-02-15 13:46:00 +0000

    Creating a function to choose operation

--- project-calculator/script.js
+++ project-calculator/script.js
@@ -1,3 +1,23 @@
+let num1, num2, operator;
+
+
+function operate(x, operator, y){
+    switch(operator){
+        case "+":
+            add(x,y);
+            break;
+        case "-":
+            subtract(x,y);
+            break;
+        case "*":
+            multipy(x,y);
+            break;
+        case "/":
+            divide(x,y);
+            break;
+    }
+}
+
 function add(x,y){
     return x + y
 }
