ac2adb00b2283134cc53e21c8f9eb0c1aaa0a2fb
sempreverde1919 <110019528+sempreverde1919@users.noreply.github.com>
2024-02-16 12:34:32 +0000

    Adding function to handle oparate buttons

--- project-calculator/script.js
+++ project-calculator/script.js
@@ -1,41 +1,59 @@
-let num1, num2, operator, displayValue;
+let operator, displayValue=0, storedValue;
 
-const display = document.querySelector('.display');
+const displayBottom = document.querySelector('.display-bottom');
+const displayTop = document.querySelector('.display-top');
 const numBttn = document.querySelectorAll('.number-button');
 const clearBtn = document.querySelector('.clear-button');
+const opBtn = document.querySelectorAll('.operator-button');
 
 numBttn.forEach(button => button.addEventListener('click', e => numberToDisplay(e.target.value)));
 
 clearBtn.addEventListener('click', () => {
-  display.textContent = "0";
+  displayTop.textContent = "";
+  displayBottom.textContent = "0";
 })
 
+opBtn.forEach(button => button.addEventListener('click', e => {
+  displayValue = +(displayBottom.textContent);
+  operator = e.target.value;
+  if(['+','-','*','/'].includes(displayTop.textContent.slice(-1))){
+    storedValue = operate(storedValue, operator, displayValue);
+    displayValue = 0;
+  } else{
+    storedValue = displayValue;
+    displayValue = 0;
+  }
+  displayTop.textContent = `${storedValue} ${operator}`;
+  displayBottom.textContent = displayValue;
+}))
+
+
+
+
 function numberToDisplay(digit){
-  if(display.textContent==="0"){
+  if(displayBottom.textContent==="0"){
     if(digit==="0"){
       return
     } else{
-      display.textContent = digit
+      displayBottom.textContent = digit
     }
   } else{
-    display.textContent += digit
+    displayBottom.textContent += digit
   }
 }
+
+
 
 function operate(x, operator, y){
     switch(operator){
         case "+":
-            add(x,y);
-            break;
+            return add(x,y);
         case "-":
-            subtract(x,y);
-            break;
+            return subtract(x,y);
         case "*":
-            multipy(x,y);
-            break;
+            return multipy(x,y);
         case "/":
-            divide(x,y);
-            break;
+            return divide(x,y);
     }
 }
 
