e1e3ac39ee26eee79a1ae69dc41993ce7f11a2eb
sempreverde1919 <110019528+sempreverde1919@users.noreply.github.com>
2024-02-21 08:42:03 +0000

    Refactoring in progress in order to have oneliner display

--- project-calculator/script.js
+++ project-calculator/script.js
@@ -1,19 +1,21 @@
-let operator, displayValue=0, storedValue;
+let operator="", displayValue=0, storedValue;
 
 const displayBottom = document.querySelector('.display-bottom');
 const displayTop = document.querySelector('.display-top');
-const numBttn = document.querySelectorAll('.number-button');
+const screen = document.querySelector('.screen');
+const numBtn = document.querySelectorAll('.number-button');
 const clearBtn = document.querySelector('.clear-button');
 const opBtn = document.querySelectorAll('.operator-button');
 const equalBtn = document.querySelector('.equal-button');
 
-numBttn.forEach(button => button.addEventListener('click', e => {
-  if(displayBottom.textContent==="0"){
+numBtn.forEach(button => button.addEventListener('click', e => {
+  handleNumbers(e.target.value, screen.textContent);
+  /*if(displayBottom.textContent==="0"){
       if(e.target.value!=="0") displayBottom.textContent = e.target.value
    } else{
     displayBottom.textContent += e.target.value
   }
-  displayValue = +(displayBottom.textContent);
+  displayValue = +(displayBottom.textContent);*/
 }));
   
 clearBtn.addEventListener('click', () => {
@@ -21,10 +23,13 @@ clearBtn.addEventListener('click', () => {
   displayValue = 0;
   displayTop.textContent = "";
   displayBottom.textContent = displayValue;
+  screen.textContent = "0";
+  operator="";
 })
 
 opBtn.forEach(button => button.addEventListener('click', e => {
-  displayValue = +(displayBottom.textContent);
+  handleOperators(e.target.value, screen.textContent);
+  /*displayValue = +(displayBottom.textContent);
   let lastChar = displayTop.textContent.slice(-1);
   if(['+','-','*','/'].includes(lastChar)){
     operator = lastChar;
@@ -36,6 +41,7 @@ opBtn.forEach(button => button.addEventListener('click', e => {
   displayTop.textContent = `${storedValue} ${operator}`;
   displayValue = 0;
   displayBottom.textContent = displayValue;
+  */
 }))
 
 equalBtn.addEventListener('click', () => {
@@ -71,4 +77,32 @@ function multiply(x, y){
 }
 function divide(x, y){
   return Math.round((x/y)*1000)/1000;
+}
+
+function handleNumbers(digit, screenText){
+  if(digit==="." && screenText.includes(".")) return;
+  if(screenText==="0") {
+    digit==="." ? screenText = "0." : screenText = digit
+  } else {
+    screenText += digit;
+  }
+  refreshScreen(screenText);
+}
+
+function handleOperators(digit, screenText){
+  displayValue = +screenText;
+  operator = digit;
+  resetScreen();
+  console.log(displayValue);
+
+  
+}
+
+
+function resetScreen(){
+  screen.textContent = "0";
+}
+
+function refreshScreen(text){
+  screen.textContent = text;
 }
