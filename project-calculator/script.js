affc27822e9abe5fa6c450b37e897dde0896b32e
sempreverde1919 <110019528+sempreverde1919@users.noreply.github.com>
2024-02-21 12:22:08 +0000

    Copying my previous solution

--- project-calculator/script.js
+++ project-calculator/script.js
@@ -1,108 +1,158 @@
-let operator="", displayValue=0, storedValue;
+const prevOutput = document.querySelector(".previous");
+const currentOutput = document.querySelector(".current");
+const numberButtons = document.querySelectorAll(".number");
+const operatorButtons = document.querySelectorAll(".operator");
+const equalButton = document.querySelector("#equal");
+const allClearButton = document.querySelector("#allclear");
+const deleteButton = document.querySelector("#delete");
+const plusMinusButton = document.querySelector("#plusminus");
 
-const displayBottom = document.querySelector('.display-bottom');
-const displayTop = document.querySelector('.display-top');
-const screen = document.querySelector('.screen');
-const numBtn = document.querySelectorAll('.number-button');
-const clearBtn = document.querySelector('.clear-button');
-const opBtn = document.querySelectorAll('.operator-button');
-const equalBtn = document.querySelector('.equal-button');
+let prevValue = "",
+  currentValue = "",
+  operator = "";
 
-numBtn.forEach(button => button.addEventListener('click', e => {
-  handleNumbers(e.target.value, screen.textContent);
-  /*if(displayBottom.textContent==="0"){
-      if(e.target.value!=="0") displayBottom.textContent = e.target.value
-   } else{
-    displayBottom.textContent += e.target.value
+function add(a, b) {
+  return a + b;
+}
+function subtract(a, b) {
+  return a - b;
+}
+function multiply(a, b) {
+  return a * b;
+}
+function divide(a, b) {
+  return a / b;
+}
+
+function operate(a, operator, b) {
+  a = Number(a);
+  b = Number(b);
+  if (operator === "/" && b === 0) {
+    return "ERROR";
   }
-  displayValue = +(displayBottom.textContent);*/
-}));
-  
-clearBtn.addEventListener('click', () => {
-  storedValue = 0;
-  displayValue = 0;
-  displayTop.textContent = "";
-  displayBottom.textContent = displayValue;
-  screen.textContent = "0";
-  operator="";
-})
-
-opBtn.forEach(button => button.addEventListener('click', e => {
-  handleOperators(e.target.value, screen.textContent);
-  /*displayValue = +(displayBottom.textContent);
-  let lastChar = displayTop.textContent.slice(-1);
-  if(['+','-','*','/'].includes(lastChar)){
-    operator = lastChar;
-    storedValue = operate(storedValue, operator, displayValue);
-  } else{
-    storedValue = displayValue;
-  } 
-  operator = e.target.value;
-  displayTop.textContent = `${storedValue} ${operator}`;
-  displayValue = 0;
-  displayBottom.textContent = displayValue;
-  */
-}))
-
-equalBtn.addEventListener('click', () => {
-  if(storedValue && operator){
-        displayTop.textContent = `${storedValue} ${operator} ${displayValue} =`;
-    displayValue = operate(storedValue, operator, displayValue);
-    displayBottom.textContent = displayValue;
+  switch (operator) {
+    case "+":
+      return add(a, b);
+    case "-":
+      return subtract(a, b);
+    case "*":
+      return multiply(a, b);
+    case "/":
+      return divide(a, b);
+    default:
+      return "ERROR";
   }
-})
-
-
-function operate(x, operator, y){
-    switch(operator){
-        case "+":
-            return add(x,y);
-        case "-":
-            return subtract(x,y);
-        case "*":
-            return multiply(x,y);
-        case "/":
-            return divide(x,y);
-    }
 }
 
-function add(x, y){
-  return Math.round((x+y)*1000)/1000;
-}
-function subtract(x, y){
-  return Math.round((x-y)*1000)/1000;
-}
-function multiply(x, y){
-  return Math.round((x*y)*1000)/1000;
-}
-function divide(x, y){
-  return Math.round((x/y)*1000)/1000;
+//append the pushed number at the end of the current value
+function appendNumber(number) {
+  if (number === "." && currentValue.includes(".")) return; //no duplicate floating point
+  if (currentValue.length >= 9) return; //fit to screen
+  currentValue += number;
 }
 
-function handleNumbers(digit, screenText){
-  if(digit==="." && screenText.includes(".")) return;
-  if(screenText==="0") {
-    digit==="." ? screenText = "0." : screenText = digit
+//add the minus sign (or remove it) before the value
+function appendMinus() {
+  if (currentValue.length > 8) return; //fit to screen
+  if (currentValue.includes("-")) {
+    currentValue = currentValue.slice(1); //removes the minus sign
   } else {
-    screenText += digit;
+    currentValue = `-${currentValue}`;
   }
-  refreshScreen(screenText);
 }
 
-function handleOperators(digit, screenText){
-  displayValue = +screenText;
-  operator = digit;
-  resetScreen();
-  console.log(displayValue);
-
-  
+//handle the operator buttons
+function chooseOperator(op) {
+  !prevValue
+    ? (prevValue = currentValue) //if an operator button was pushed for the first time
+    : (prevValue = operate(prevValue, operator, currentValue)); //if an operator button was not pushed for the first time - do the calculation
+  currentValue = "";
+  operator = op; //set the operator pushed lastly
 }
 
-
-function resetScreen(){
-  screen.textContent = "0";
+//handle the equal button - do the math
+function equals() {
+  if (prevValue && currentValue) {
+    currentValue = operate(prevValue, operator, currentValue);
+    prevValue = "";
+    operator = "";
+  }
 }
 
-function refreshScreen(text){
-  screen.textContent = text;
+//handle the AC button - empty every value
+function clearAll() {
+  currentValue = "";
+  prevValue = "";
+  operator = "";
 }
+
+//handle the DEL button - remove one character from the end of the value
+function deleteOneBack() {
+  currentValue = currentValue.slice(0, -1);
+}
+
+//update the values presented on the screen
+function updateDisplay() {
+  prevOutput.innerText = `${prevValue} ${operator}`;
+  currentOutput.innerText = currentValue;
+}
+
+//if the user is pushing the keyboard key
+function handleKeyboard(key) {
+  if (+key >= 0 || key === ".") {
+    appendNumber(key);
+  }
+  if (key === "+" || key === "-" || key === "*" || key === "/") {
+    chooseOperator(key);
+  }
+  if (key === "Enter") {
+    equals();
+  }
+  if (key === "Backspace") {
+    deleteOneBack();
+  }
+  if (key === "Delete") {
+    clearAll();
+  }
+}
+
+//------------------------------------------
+
+numberButtons.forEach((button) =>
+  button.addEventListener("click", () => {
+    appendNumber(button.innerText);
+    updateDisplay();
+  })
+);
+
+operatorButtons.forEach((button) =>
+  button.addEventListener("click", () => {
+    chooseOperator(button.innerText);
+    updateDisplay();
+  })
+);
+
+equalButton.addEventListener("click", () => {
+  equals();
+  updateDisplay();
+});
+
+allClearButton.addEventListener("click", () => {
+  clearAll();
+  updateDisplay();
+});
+
+deleteButton.addEventListener("click", () => {
+  deleteOneBack();
+  updateDisplay();
+});
+
+plusMinusButton.addEventListener("click", () => {
+  appendMinus();
+  updateDisplay();
+});
+
+document.addEventListener("keyup", (e) => {
+  handleKeyboard(e.key);
+  updateDisplay();
+});
